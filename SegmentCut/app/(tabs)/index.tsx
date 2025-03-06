80b70e34448ed05a8d06ac2bbd71056372b02c6f
Lawrence Ng <surpika2020@gmail.com>
2025-03-06 10:51:02 -0800

    set picked image as background image

--- SegmentCut/app/(tabs)/index.tsx
+++ SegmentCut/app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
 import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+import { useState } from 'react';
+
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
-import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('@/assets/images/caius.jpeg');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
@@ -15,7 +20,7 @@ export default function Index() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image.');
     }
@@ -24,7 +29,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
