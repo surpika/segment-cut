90edd5d120f1dc3b4043342dd64089bf83e28c3a
Lawrence Ng <surpika2020@gmail.com>
2025-03-06 10:22:43 -0800

    add buttons

--- SegmentCut/app/(tabs)/index.tsx
+++ SegmentCut/app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-import ImageViewer from '@/components/ImageViewer'; 
+import Button from '@/components/Button'; 
+import ImageViewer from '@/components/ImageViewer';
 
-const PlaceholderImage = require('@/assets/images/caius.jpeg');
+const PlaceholderImage = require("@/assets/images/caius.jpeg");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -22,5 +27,10 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
